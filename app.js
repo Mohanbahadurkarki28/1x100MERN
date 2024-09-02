6359f8183b734ccee65c2ef09503a1e9c9a909a1
mohankarki28 <mohankarki86555@gmail.com>
2024-09-02 17:30:52 +0545

    fist

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+app.get("/abc", (req,res) => {res.status(400).json({data : "Hello world"})});
+app.get("/home", (req,res) => {res.send("Hello from home")});
+app.get("/about", (req,res) => {res.send("Hello from about")});
+app.use((req,res) =>{
+    res.sendFile(__dirname+"/public/404.html")
+})
+
+app.listen(3000 , ()=> console.log("server running at port 3000"));
