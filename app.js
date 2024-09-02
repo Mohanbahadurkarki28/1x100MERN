0de1b48df962826a4f3e348db308d80f8da7aec8
mohankarki28 <mohankarki86555@gmail.com>
2024-09-02 17:46:12 +0545

    first commit

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 
 app.get("/abc", (req,res) => {res.status(400).json({data : "Hello world"})});
 app.get("/home", (req,res) => {res.send("Hello from home")});
+app.get("/Contact", (req,res) => {res.send("Hello from Contact")});
 app.get("/about", (req,res) => {res.send("Hello from about")});
 app.use((req,res) =>{
     res.sendFile(__dirname+"/public/404.html")
